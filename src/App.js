3ae83f5c162979bee8d4f74813e8daf7aa465576
sue2021 <carpentersuzanne@johndeere.com>
2021-10-03 17:37:15 -0500

    Commit for app.js

--- src/App.js
+++ src/App.js
@@ -4,35 +4,31 @@ import axios from 'axios';
 import member_detail from './components/member_detail';
 import members from './components/members';
 import contact from './components/contact';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor() {
     super();
-    this.apiUrl = 'http://localhost:3000/api/users'
+    this.apiUrl = 'http://localhost:3000/api/users/';
     this.state = {
-      members: []
+      users: []
 
-    }
+   }
   }
 
   async componentDidMount() {
-    const response = await axios.get(this.apiUrl)
-    this.setState({members: response.data.users})
+    const response = await axios.get(this.apiUrl);
+    this.setState({users: response.data.users})
+ 
   }
-  addMember = async () => {};
-
+  addUser = async () => {};
+  
   render() { 
   return (
     <div className="App">
-      <h1>Welcom to St. Anthony's Grief Group</h1>
-      <switch>
-        <Route path="/members" component={members} />
-        <Route path="/details" component={member_detail} />
-        <Route path="/contact" component={contact} />
-      </switch>
+      <h1>Welcome to St. Anthony's Grief Group</h1>
+   
 
-     
     </div>
   );
 }
--- src/components/members.js
+++ src/components/members.js
@@ -1,10 +1,23 @@
 import { Route, Link } from "react-router-dom";
 import React from "react";
-import './members.css'
+import './members.css';
+
 export default function members(props) {
+    const members = props.api.users.map((members) => {
+      return (
+        <li key={members.name}>
+          <Link to={'/members/${members.name}'}></Link>
+        </li>
+      );
+    });
+  
+        
+        
+    
     return (
     <div className="members">
         <h2>Grief Group list</h2>
+        <ul>{members}</ul>
     </div>
     );
 }
